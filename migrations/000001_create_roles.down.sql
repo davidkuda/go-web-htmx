1aaa465bcc2a5833128e5fe2d1d614a5f9f3a641
David Kuda <davidkuda3@gmail.com>
2026-03-27 21:17:38 +0100

    WIP: update roles up and down

--- migrations/000001_create_roles.down.sql
+++ migrations/000001_create_roles.down.sql
@@ -1,7 +1,27 @@
-ALTER DATABASE kuda_ai OWNER TO davidkuda;
-DROP ROLE dev;
+begin;
+
+drop role gokanban;
+drop role david;
+
+revoke all on schema gokanban from application;
+
+alter default privileges
+for role developer
+in schema gokanban
+revoke all on tables from application;
+
+alter default privileges
+for role developer
+in schema gokanban
+revoke all on sequences from application;
+
+drop role application;
+
+alter database gokanban OWNER TO transcribo;
+
 DROP OWNED BY developer;
-DROP ROLE developer;
-DROP OWNED BY kuda_ai;
-DROP ROLE kuda_ai;
-DROP ROLE app;
+
+drop role developer;
+drop schema gokanban;
+
+commit;
--- migrations/000001_create_roles.up.sql
+++ migrations/000001_create_roles.up.sql
@@ -1,14 +1,22 @@
--- prerequisite: createdb davidkuda
+-- prerequisite: createdb gokanban
+
+-- developers inherit from group developer.
+-- go web app runs with bellevue which inherits from application.
+-- migrations should be run with developer.
 
 BEGIN;
 
 create schema gokanban;
 
+ALTER DATABASE gokanban
+SET search_path = gokanban, public;
+
 --------------------------------------------------------------------
 -- Roles: Groups: Developer ----------------------------------------
--- A developer can CREATE ON SCHEMA, an app can only USAGE. --------
 
 create role developer with nologin;
+
+alter schema gokanban owner to developer;
 
 grant create on schema gokanban to developer;
 
@@ -20,13 +28,39 @@ TO developer;
 --------------------------------------------------------------------
 -- Roles: Groups: App ----------------------------------------------
 
-create role app with nologin;
+create role application with nologin;
 
-grant usage on schema gokanban to app;
+grant usage on schema gokanban to application;
 
 grant select, insert, update, delete 
 on all tables in schema gokanban
-to app;
+to application;
+
+
+--------------------------------------------------------------------
+-- Default privileges: developer => application: -------------------
+
+-- every time developer creates a new table, application will
+-- receive a grant as specified in:
+ALTER DEFAULT PRIVILEGES
+FOR ROLE developer
+IN SCHEMA gokanban
+GRANT SELECT, INSERT, UPDATE, DELETE
+ON TABLES
+TO application;
+
+-- also consider sequences:
+-- USAGE:  allows nextval(), currval(), lastval()
+-- SELECT: allows currval() and reading the sequence
+--         via SELECT directly.
+-- UPDATE: allows nextval() and setval() – modifying
+--         the sequence’s current value.
+ALTER DEFAULT PRIVILEGES
+FOR ROLE developer
+IN SCHEMA gokanban
+GRANT USAGE, SELECT, UPDATE
+ON SEQUENCES
+TO application;
 
 
 --------------------------------------------------------------------
@@ -53,7 +87,6 @@ BEGIN
     RAISE NOTICE 'Generated password: %', pass;
 END $$;
 
-GRANT app TO gokanban;
+GRANT application TO gokanban;
 
-
-ALTER SCHEMA gokanban OWNER TO developer;
+COMMIT;
